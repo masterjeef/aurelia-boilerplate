7fde1699e0aedbd8b896117b2f54c8a3bf93cce5
Jeff Linton <jslinton44@gmail.com>
2018-01-17 22:34:04 -0700

    Adds the tsconfig, sets up the webpack config (project compiles)

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,7 +1,40 @@
 import * as webpack from 'webpack';
+import * as path from 'path';
+
+const outputDirectory = 'dist';
 
 const webpackConfig: webpack.Configuration = {
-    
+    entry: 'aurelia-bootstrapper',
+    context: path.resolve(__dirname),
+    output: {
+        path: path.resolve(__dirname, outputDirectory),
+        publicPath: '/',
+        filename: '[name].bundle.js',
+        chunkFilename: '[name].chunk.js'
+    },
+    devtool: 'source-map',
+    watch: true,
+    module: {
+        rules: [
+            {
+                test: /\.html?$/,
+                use: 'html-loader'
+            },
+            {
+                test: /\.ts?$/,
+                use: ['babel-loader', 'ts-loader']
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    devServer: {
+        inline: true,
+        contentBase: '/',
+        historyApiFallback: true,
+        port: 8080
+    }
 }
 
 export default webpackConfig;
